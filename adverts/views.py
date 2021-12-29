a848a3ff0d2ee7851bc47bf7e715429fe3072f93
FJ-77 <brawnd@list.ru>
2021-12-29 16:35:28 +0600

    Доделал вьюшку

--- adverts/views.py
+++ adverts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from .models import Advert
 
 def index(request):
-		
-
-	return render(request, 'adverts/index.html')
+	adverts = Advert.objetcs.all()
+	context = { 'adverts':adverts }
+	return render(request, 'adverts/index.html', context)
 
 
 
