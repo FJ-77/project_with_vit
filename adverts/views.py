0f0ab6d0d02fd8e62ddc1477df42da223d70c4bc
FJ-77 <brawnd@list.ru>
2021-12-29 16:49:03 +0600

    Сделал вторую вьюшку advert_detail

--- adverts/views.py
+++ adverts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Advert
 
-def index(request):
+def adverts(request):
 	adverts = Advert.objetcs.all()
 	context = { 'adverts':adverts }
 	return render(request, 'adverts/index.html', context)
 
-
-
+def advert_detail(request, pk):
+	Advert.objects.get(pk=pk)
+	context = { 'advert':advert }
+	return render(request, 'adverts/advert_detail.html', context)
